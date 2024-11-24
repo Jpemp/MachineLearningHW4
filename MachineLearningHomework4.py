111189353f97733fdd65e3bdcdeb600318d22354
Jpemp <159839779+Jpemp@users.noreply.github.com>
2024-11-24 14:14:43 -0600

    Update MachineLearningHomework4.py

--- MachineLearningHomework4.py
+++ MachineLearningHomework4.py
@@ -1,6 +1,8 @@
 from ucimlrepo import fetch_ucirepo 
 import pandas as pd
 import math
+import matplotlib.pyplot as plt
+import numpy as np
 
 #Problem 1
 #Dataset fetching
@@ -23,37 +25,50 @@ def mean(X):
     mean = (1/total) * X.sum()
     return mean
 
-def standard_dev(X, X_m):
+def standard_dev(X,X_m):
     total = len(X)
     var = (1/(total-1)) * ((X-X_m) ** 2)
     s_var = var.sum()
     sd = (s_var ** 0.5)
     return sd
 
+#Z-score calculation
 X_m = mean(X)
-
 X_sd = standard_dev(X, X_m)
-
 X_z = (X-X_m)/X_sd
 
-#Display X and Y after Z-score normalization
+#Display X after Z-score normalization, along with Y
 print(X_z)
 print(Y)
 
 #Problem 2
 
-def softmax():
-    print(1)
-    return 0
+def softmax(X):
+    e = math.exp(X)
+    soft = e/e.sum()
+    return soft
 
-def kfold_split():
-    print(2)
-    return 0
+def kfold_split(X, k):
+    X_5_split = np.array_split(X,k)
+    return X_5_split
 
-def accuracy():
-    print(3)
-    return 0
+def accuracy(Y_true, Y_pred):
+    accurate_pred = 0
+    total = len(Y_true)
+
+    for i in range(total):
+        if Y_true[i] == Y_pred[i]:
+            accurate_pred = accurate_pred + 1
+    acc = accurate_pred/total
+    return acc
 
 def confusion_matrix():
-    print(4)
-    return 0
+    
+
+    return c_m
+
+ks = kfold_split(X,5) 
+
+#Problem 3
+
+#Problem 4
