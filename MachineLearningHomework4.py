859c664b05360d253616f9b550f025a40e0846af
Jpemp <159839779+Jpemp@users.noreply.github.com>
2024-11-24 12:50:57 -0600

    Update MachineLearningHomework4.py

--- MachineLearningHomework4.py
+++ MachineLearningHomework4.py
@@ -1,5 +1,6 @@
 from ucimlrepo import fetch_ucirepo 
 import pandas as pd
+import math
 
 #Problem 1
 #Dataset fetching
@@ -17,9 +18,42 @@ Y = Y.head(40)
 print(X)
 print(Y)
 
-X = (X-X.mean())/X.std()
+def mean(X):
+    total = len(X)
+    mean = (1/total) * X.sum()
+    return mean
+
+def standard_dev(X, X_m):
+    total = len(X)
+    var = (1/(total-1)) * ((X-X_m) ** 2)
+    s_var = var.sum()
+    sd = (s_var ** 0.5)
+    return sd
+
+X_m = mean(X)
+
+X_sd = standard_dev(X, X_m)
+
+X_z = (X-X_m)/X_sd
 
 #Display X and Y after Z-score normalization
-print(X)
+print(X_z)
+print(Y)
 
 #Problem 2
+
+def softmax():
+    print(1)
+    return 0
+
+def kfold_split():
+    print(2)
+    return 0
+
+def accuracy():
+    print(3)
+    return 0
+
+def confusion_matrix():
+    print(4)
+    return 0
